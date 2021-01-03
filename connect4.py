ed2ae7e220ecc366dcd9c6c88e5d64b0ee30b87f
Anzle <robleewilliams3@gmail.com>
2021-01-02 16:30:13 -0800

    Added Game state

--- connect4.py
+++ connect4.py
@@ -5,6 +5,7 @@ class Connect4:
         self.EMPTY = ' '
         self.board = []
         self.player = 'R'
+        self.state = 'Unfinished'
 
         self._cols = 7
         self._rows = 6
@@ -16,7 +17,10 @@ class Connect4:
     def make_move(self, column: int) -> bool:
         """Returns True if able to place piece in current player's column"""
         column -= 1 # drecement for code
-        if not column in range(0,7) or self.board[column][5] != self.EMPTY:
+        if (
+            self.state != 'Unfinished'
+            or not column in range(0,7) 
+            or self.board[column][5] != self.EMPTY):
             return False
         
         # get lowest height in column
@@ -30,13 +34,14 @@ class Connect4:
 
     def __str__(self) -> str:
         """Return's the board as a String"""
-        board_str = 'Current Player: '+self.player+'\n  1   2   3   4   5   6   7  \n'
+        board_str = 'Current Player: '+self.player + '\n  1   2   3   4   5   6   7  \n'
         for y in range(self._rows-1, -1, -1):
             for x in range(self._cols):
                 board_str += '| ' + self.board[x][y] + ' '
             board_str += '|\n'
         board_str += '-'*29
-        return board_str
+        board_str += '\nGame State: '+ self.state
+        return board_str + '\n'
 
     def __repr__(self) -> str:
         """Return the Connect4 game state as a String"""
